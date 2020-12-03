3e559762e7b6bb8ec625d39c759cb67c489a4b80
Katia Duarte <katiaipduarte@hotmail.com>
2020-12-03 20:00:34 +0000

    add sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Hello from './components/Hello';
+import './index.scss';
 
 const App = (): JSX.Element => {
   return (
-    <div>
+    <div className="color">
       <Hello compiler="parcel" framework="123" />
     </div>
   );
